4515c6cd5132fd215ad6ae29eaa0bf48e36fc2f8
Alexander Batalov <alex.batalov@gmail.com>
2022-06-18 16:47:23 +0300

    Cleanup game_memory.h
    See #42

--- src/game_memory.cc
+++ src/game_memory.cc
@@ -3,7 +3,12 @@
 #include "db.h"
 #include "dictionary.h"
 #include "memory.h"
+#include "memory_defs.h"
 #include "memory_manager.h"
+
+static void* gameMemoryMalloc(size_t size);
+static void* gameMemoryRealloc(void* ptr, size_t newSize);
+static void gameMemoryFree(void* ptr);
 
 // 0x44B250
 int gameMemoryInit()
@@ -16,19 +21,19 @@ int gameMemoryInit()
 }
 
 // 0x44B294
-void* gameMemoryMalloc(size_t size)
+static void* gameMemoryMalloc(size_t size)
 {
     return internal_malloc(size);
 }
 
 // 0x44B29C
-void* gameMemoryRealloc(void* ptr, size_t newSize)
+static void* gameMemoryRealloc(void* ptr, size_t newSize)
 {
     return internal_realloc(ptr, newSize);
 }
 
 // 0x44B2A4
-void gameMemoryFree(void* ptr)
+static void gameMemoryFree(void* ptr)
 {
     internal_free(ptr);
 }
--- src/game_memory.h
+++ src/game_memory.h
@@ -1,11 +1,6 @@
 #ifndef GAME_MEMORY_H
 #define GAME_MEMORY_H
 
-#include "memory_defs.h"
-
 int gameMemoryInit();
-void* gameMemoryMalloc(size_t size);
-void* gameMemoryRealloc(void* ptr, size_t newSize);
-void gameMemoryFree(void* ptr);
 
 #endif /* GAME_MEMORY_H */
