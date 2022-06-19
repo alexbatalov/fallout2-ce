3625d4cdbc72d50cbb3dcd27eb744da606db5778
k3tamina <k3tak3ta@gmail.com>
2022-06-19 09:40:45 +0200

    Cleanup game_mouse.h

--- src/game_mouse.h
+++ src/game_mouse.h
@@ -1,16 +1,7 @@
 #ifndef GAME_MOUSE_H
 #define GAME_MOUSE_H
 
-#include "art.h"
-#include "geometry.h"
 #include "obj_types.h"
-
-typedef enum ScrollableDirections {
-    SCROLLABLE_W = 0x01,
-    SCROLLABLE_E = 0x02,
-    SCROLLABLE_N = 0x04,
-    SCROLLABLE_S = 0x08,
-} ScrollableDirections;
 
 typedef enum GameMouseMode {
     GAME_MOUSE_MODE_MOVE,
@@ -75,73 +66,10 @@ typedef enum MouseCursorType {
     FIRST_GAME_MOUSE_ANIMATED_CURSOR = MOUSE_CURSOR_WAIT_PLANET,
 } MouseCursorType;
 
-extern bool gGameMouseInitialized;
-extern int _gmouse_enabled;
-extern int _gmouse_mapper_mode;
-extern int _gmouse_click_to_scroll;
-extern int _gmouse_scrolling_enabled;
-extern int gGameMouseCursor;
-extern CacheEntry* gGameMouseCursorFrmHandle;
-extern const int gGameMouseCursorFrmIds[MOUSE_CURSOR_TYPE_COUNT];
-extern bool gGameMouseObjectsInitialized;
-extern bool _gmouse_3d_hover_test;
-extern unsigned int _gmouse_3d_last_move_time;
-extern Art* gGameMouseActionMenuFrm;
-extern CacheEntry* gGameMouseActionMenuFrmHandle;
-extern int gGameMouseActionMenuFrmWidth;
-extern int gGameMouseActionMenuFrmHeight;
-extern int gGameMouseActionMenuFrmDataSize;
-extern int _gmouse_3d_menu_frame_hot_x;
-extern int _gmouse_3d_menu_frame_hot_y;
-extern unsigned char* gGameMouseActionMenuFrmData;
-extern Art* gGameMouseActionPickFrm;
-extern CacheEntry* gGameMouseActionPickFrmHandle;
-extern int gGameMouseActionPickFrmWidth;
-extern int gGameMouseActionPickFrmHeight;
-extern int gGameMouseActionPickFrmDataSize;
-extern int _gmouse_3d_pick_frame_hot_x;
-extern int _gmouse_3d_pick_frame_hot_y;
-extern unsigned char* gGameMouseActionPickFrmData;
-extern Art* gGameMouseActionHitFrm;
-extern CacheEntry* gGameMouseActionHitFrmHandle;
-extern int gGameMouseActionHitFrmWidth;
-extern int gGameMouseActionHitFrmHeight;
-extern int gGameMouseActionHitFrmDataSize;
-extern unsigned char* gGameMouseActionHitFrmData;
-extern Art* gGameMouseBouncingCursorFrm;
-extern CacheEntry* gGameMouseBouncingCursorFrmHandle;
-extern int gGameMouseBouncingCursorFrmWidth;
-extern int gGameMouseBouncingCursorFrmHeight;
-extern int gGameMouseBouncingCursorFrmDataSize;
-extern unsigned char* gGameMouseBouncingCursorFrmData;
-extern Art* gGameMouseHexCursorFrm;
-extern CacheEntry* gGameMouseHexCursorFrmHandle;
-extern int gGameMouseHexCursorFrmWidth;
-extern int gGameMouseHexCursorHeight;
-extern int gGameMouseHexCursorDataSize;
-extern unsigned char* gGameMouseHexCursorFrmData;
-extern unsigned char gGameMouseActionMenuItemsLength;
-extern unsigned char* _gmouse_3d_menu_actions_start;
-extern unsigned char gGameMouseActionMenuHighlightedItemIndex;
-extern const short gGameMouseActionMenuItemFrmIds[GAME_MOUSE_ACTION_MENU_ITEM_COUNT];
-extern int _gmouse_3d_modes_enabled;
-extern int gGameMouseMode;
-extern int gGameMouseModeFrmIds[GAME_MOUSE_MODE_COUNT];
-extern const int gGameMouseModeSkills[GAME_MOUSE_MODE_SKILL_COUNT];
-extern int gGameMouseAnimatedCursorNextFrame;
-extern unsigned int gGameMouseAnimatedCursorLastUpdateTimestamp;
-extern int _gmouse_bk_last_cursor;
-extern bool gGameMouseItemHighlightEnabled;
-extern Object* gGameMouseHighlightedItem;
 extern bool _gmouse_clicked_on_edge;
-extern int dword_518D9C;
 
-extern int gGameMouseActionMenuItems[GAME_MOUSE_ACTION_MENU_ITEM_COUNT];
-extern int gGameMouseLastX;
-extern int gGameMouseLastY;
 extern Object* gGameMouseBouncingCursor;
 extern Object* gGameMouseHexCursor;
-extern Object* gGameMousePointedObject;
 
 int gameMouseInit();
 int gameMouseReset();
@@ -150,40 +78,24 @@ void _gmouse_enable();
 void _gmouse_disable(int a1);
 void _gmouse_enable_scrolling();
 void _gmouse_disable_scrolling();
-int _gmouse_get_click_to_scroll();
 int _gmouse_is_scrolling();
 void gameMouseRefresh();
 void _gmouse_handle_event(int mouseX, int mouseY, int mouseState);
 int gameMouseSetCursor(int cursor);
 int gameMouseGetCursor();
-void _gmouse_3d_enable_modes();
 void gameMouseSetMode(int a1);
 int gameMouseGetMode();
 void gameMouseCycleMode();
 void _gmouse_3d_refresh();
-int gameMouseSetBouncingCursorFid(int fid);
 void gameMouseResetBouncingCursorFid();
 void gameMouseObjectsShow();
 void gameMouseObjectsHide();
 bool gameMouseObjectsIsVisible();
-Object* gameMouseGetObjectUnderCursor(int objectType, bool a2, int elevation);
 int gameMouseRenderPrimaryAction(int x, int y, int menuItem, int width, int height);
 int _gmouse_3d_pick_frame_hot(int* a1, int* a2);
 int gameMouseRenderActionMenuItems(int x, int y, const int* menuItems, int menuItemsCount, int width, int height);
 int gameMouseHighlightActionMenuItemAtIndex(int menuItemIndex);
-int gameMouseRenderAccuracy(const char* string, int color);
-int gameMouseRenderActionPoints(const char* string, int color);
 void gameMouseLoadItemHighlight();
-int gameMouseObjectsInit();
-int gameMouseObjectsReset();
-void gameMouseObjectsFree();
-int gameMouseActionMenuInit();
-void gameMouseActionMenuFree();
-int gameMouseUpdateHexCursorFid(Rect* rect);
-int _gmouse_3d_move_to(int x, int y, int elevation, Rect* a4);
-int gameMouseHandleScrolling(int x, int y, int cursor);
 void _gmouse_remove_item_outline(Object* object);
-int objectIsDoor(Object* object);
-bool gameMouseClickOnInterfaceBar();
 
 #endif /* GAME_MOUSE_H */
