c928a95b02a8b6d7f1019bc2222d1ff304852563
k3tamina <k3tak3ta@gmail.com>
2022-06-26 22:43:26 +0200

    Cleanup widget.h

--- src/widget.h
+++ src/widget.h
@@ -1,19 +1,11 @@
 #ifndef WIDGET_H
 #define WIDGET_H
 
-extern const float flt_50EB1C;
-extern const float flt_50EB20;
-
-extern int _updateRegions[32];
-
-void _showRegion(int a1);
 int _update_widgets();
 int widgetGetFont();
 int widgetSetFont(int a1);
-int widgetGetTextFlags();
 int widgetSetTextFlags(int a1);
 unsigned char widgetGetTextColor();
-unsigned char widgetGetHighlightColor();
 int widgetSetTextColor(float a1, float a2, float a3);
 int widgetSetHighlightColor(float a1, float a2, float a3);
 
