0f60556b73b7896086b3a2682dc9a1f060cc57fc
Alexander Batalov <alex.batalov@gmail.com>
2022-08-16 14:14:53 +0300

    Decompile some input functions

--- src/core.cc
+++ src/core.cc
@@ -17,11 +17,11 @@
 #include <limits.h>
 #include <string.h>
 
-// NOT USED.
-void (*_idle_func)() = NULL;
+// 0x51E234
+IdleFunc* _idle_func = NULL;
 
-// NOT USED.
-void (*_focus_func)(int) = NULL;
+// 0x51E238
+FocusFunc* _focus_func = NULL;
 
 // 0x51E23C
 int gKeyboardKeyRepeatRate = 80;
@@ -922,6 +922,70 @@ unsigned int _get_bk_time()
     return gTickerLastTimestamp;
 }
 
+// NOTE: Unused.
+//
+// 0x4C9418
+void inputSetKeyboardKeyRepeatRate(int value)
+{
+    gKeyboardKeyRepeatRate = value;
+}
+
+// NOTE: Unused.
+//
+// 0x4C9420
+int inputGetKeyboardKeyRepeatRate()
+{
+    return gKeyboardKeyRepeatRate;
+}
+
+// NOTE: Unused.
+//
+// 0x4C9428
+void inputSetKeyboardKeyRepeatDelay(int value)
+{
+    gKeyboardKeyRepeatDelay = value;
+}
+
+// NOTE: Unused.
+//
+// 0x4C9430
+int inputGetKeyboardKeyRepeatDelay()
+{
+    return gKeyboardKeyRepeatDelay;
+}
+
+// NOTE: Unused.
+//
+// 0x4C9438
+void inputSetFocusFunc(FocusFunc* func)
+{
+    _focus_func = func;
+}
+
+// NOTE: Unused.
+//
+// 0x4C9440
+FocusFunc* inputGetFocusFunc()
+{
+    return _focus_func;
+}
+
+// NOTE: Unused.
+//
+// 0x4C9448
+void inputSetIdleFunc(IdleFunc* func)
+{
+    _idle_func = func;
+}
+
+// NOTE: Unused.
+//
+// 0x4C9450
+IdleFunc* inputGetIdleFunc()
+{
+    return _idle_func;
+}
+
 // 0x4C9490
 void buildNormalizedQwertyKeys()
 {
@@ -1375,7 +1439,7 @@ void _GNW95_process_key(KeyboardData* data)
 void _GNW95_lost_focus()
 {
     if (_focus_func != NULL) {
-        _focus_func(0);
+        _focus_func(false);
     }
 
     while (!gProgramIsActive) {
@@ -1387,7 +1451,7 @@ void _GNW95_lost_focus()
     }
 
     if (_focus_func != NULL) {
-        _focus_func(1);
+        _focus_func(true);
     }
 }
 
