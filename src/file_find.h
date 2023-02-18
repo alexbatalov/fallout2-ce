81b934530319fd102c8d3eef05503d08658652f4
Alexander Batalov <alex.batalov@gmail.com>
2023-02-18 19:06:20 +0300

    Update config

--- src/file_find.h
+++ src/file_find.h
@@ -2,8 +2,6 @@
 #define FILE_FIND_H
 
 #if defined(_WIN32)
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
 #include <windows.h>
 #else
 #include <dirent.h>
--- src/platform_compat.cc
+++ src/platform_compat.cc
@@ -3,8 +3,6 @@
 #include <string.h>
 
 #ifdef _WIN32
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
 #include <windows.h>
 #endif
 
