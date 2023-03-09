31998d65c74dceb9b6cf5ab6f1304fb439c71634
Vasilii Rogin <vasya@rogin.ru>
2023-03-09 10:29:45 +0200

    Reading IDBFS

---
+++ os/web/menu.js
@@ -0,0 +1,122 @@
+// @ts-check
+// @filename: types.d.ts
+
+/** @returns {Promise<IDBDatabase>} */
+async function initDb() {
+    const IDBFS_DB_VERSION = 21;
+
+    return new Promise((resolve, reject) => {
+        const request = window.indexedDB.open(
+            "/app/data/SAVEGAME",
+            IDBFS_DB_VERSION
+        );
+        request.onerror = (event) => {
+            reject(request.error);
+        };
+        request.onsuccess = (event) => {
+            const database = request.result;
+            resolve(database);
+        };
+        request.onupgradeneeded = (event) => {
+            reject(new Error(`LOL IDBFS version bumped!`));
+            throw new Error(`Not implemented`);
+        };
+    });
+}
+
+const IDBFS_STORE_NAME = "FILE_DATA";
+
+/**
+ * @typedef { {
+ *     mode: number,
+ *     timestamp:Date
+ *     contents?: Int8Array
+ *   } } IdbFileData
+ */
+
+/**
+ * @param {IDBDatabase} database
+ * @returns {Promise<Map<IDBValidKey, IdbFileData>>}
+ */
+async function readFilesFromDb(database) {
+    return await new Promise((resolve, reject) => {
+        const objectStore = database
+            .transaction(IDBFS_STORE_NAME, "readonly")
+            .objectStore(IDBFS_STORE_NAME);
+        const cursorReq = objectStore.openCursor();
+
+        const newMap = new Map();
+        cursorReq.onsuccess = (event) => {
+            const cur = cursorReq.result;
+            if (!cur) {
+                resolve(newMap);
+                return;
+            }
+            const key = cur.key;
+            const value = cur.value;
+            newMap.set(key, value);
+            cur.continue();
+        };
+        cursorReq.onerror = (event) => {
+            reject(cursorReq.error);
+        };
+    });
+}
+
+/**
+ *
+ * @param {Map<IDBValidKey, IdbFileData>} files
+ */
+function renderSlots(files) {
+    const container = document.getElementById("slots_container");
+    if (!container) {
+        throw new Error(`No container`);
+    }
+    container.innerHTML = "";
+    for (let i = 1; i <= 10; i++) {
+        const slotId = ("0" + i.toString()).slice(-2);
+        const slotDiv = document.createElement("div");
+        container.appendChild(slotDiv);
+
+        const saveDat = files.get(`/app/data/SAVEGAME/SLOT${slotId}/SAVE.DAT`);
+        if (!saveDat || !saveDat.contents) {
+            slotDiv.innerHTML = `No slot${slotId}`;
+            continue;
+        }
+
+        const expectedHeader = "FALLOUT SAVE FILE";
+        const observedHeader = String.fromCharCode(
+            ...saveDat.contents.slice(0, expectedHeader.length)
+        );
+        if (expectedHeader !== observedHeader) {
+            slotDiv.innerHTML = `Error with slot${slotId}`;
+            continue;
+        };
+
+        const saveName = String.fromCharCode(
+            ...saveDat.contents.slice(0x3d, Math.min(0x3d + 0x1e, saveDat.contents.indexOf(0, 0x3d)) )
+        );
+
+        slotDiv.innerHTML = `'${saveName}'`;
+    }
+}
+
+function setStatus(msg) {
+    const el = document.getElementById("status_text");
+    if (!el) {
+        throw new Error(`No status text div`);
+    }
+    el.innerHTML = msg;
+}
+(async () => {
+    setStatus(`Loading database`);
+    const database = await initDb();
+    const files = await readFilesFromDb(database);
+
+    renderSlots(files);
+
+    setStatus("Ready");
+})().catch((e) => {
+    console.warn(e);
+    setStatus(`${e.name} ${e.message}`);
+});
