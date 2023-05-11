cfe4bc5195b685a3166676bd0423a3da26fd0fd3
Vasilii Rogin <vasya@rogin.ru>
2023-05-11 22:04:00 +0300

    Update config

--- os/web/config.js
+++ os/web/config.js
@@ -1,7 +1,20 @@
-const gamesConfig = [{
-    name: "Fallout: Nevada",
-    folder: "FalloutOfNevada",
-    links: ["http://fallout-nevada.ucoz.ru/", "https://vk.com/rebuild_by_foxx"]
-}];
+const gamesConfig = [
+    {
+        name: "Fallout: Nevada",
+        folder: "FalloutOfNevada",
+        links: [
+            "http://fallout-nevada.ucoz.ru/",
+            "https://vk.com/rebuild_by_foxx",
+        ],
+    },
+    {
+        name: "Fallout: Nevada English",
+        folder: "FalloutNevadaEng",
+        links: [
+            "https://www.nma-fallout.com/threads/fallout-nevada-extended.207311/",
+            "http://fallout-nevada.ucoz.ru/",
+        ],
+    },
+];
 
 const useGzip = true;
