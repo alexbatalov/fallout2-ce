71f84cbe448e09f62d4efbe62e8c9caaeb300d71
phobos2077 <phobos2077@gmail.com>
2024-05-03 16:22:18 +0200

    Decypher buildFid (art_id) function

--- src/art.cc
+++ src/art.cc
@@ -1006,45 +1006,28 @@ static void artCacheFreeImpl(void* ptr)
     internal_free(ptr);
 }
 
-// 0x419C88
-int buildFid(int objectType, int frmId, int animType, int a3, int rotation)
+static int buildFidInternal(unsigned short frmId, unsigned char weaponCode, unsigned char animType, unsigned char objectType, unsigned char rotation)
 {
-    int v7, v8, v9, v10;
+    return ((rotation << 28) & 0x70000000) | (objectType << 24) | ((animType << 16) & 0xFF0000) | ((weaponCode << 12) & 0xF000) | (frmId & 0xFFF);
+}
 
-    v10 = rotation;
-
-    if (objectType != OBJ_TYPE_CRITTER) {
-        goto zero;
+// 0x419C88
+int buildFid(int objectType, int frmId, int animType, int weaponCode, int rotation)
+{
+    // Always use rotation 0 (NE) for non-critters, for certain critter animations.
+    // For other critter animations, check if art for the given rotation exists, if not try rotation 1 (E) and if that also doesn't exist, then default to 0 (NE).
+    if (objectType != OBJ_TYPE_CRITTER
+        || animType == ANIM_FIRE_DANCE
+        || animType < ANIM_FALL_BACK
+        || animType > ANIM_FALL_FRONT_BLOOD) {
+        rotation = ROTATION_NE;
+    } else if (!artExists(buildFidInternal(frmId, weaponCode, animType, OBJ_TYPE_CRITTER, rotation))) {
+        rotation = rotation != ROTATION_E
+                && artExists(buildFidInternal(frmId, weaponCode, animType, OBJ_TYPE_CRITTER, ROTATION_E))
+            ? ROTATION_E
+            : ROTATION_NE;
     }
-
-    if (animType == ANIM_FIRE_DANCE || animType < ANIM_FALL_BACK || animType > ANIM_FALL_FRONT_BLOOD) {
-        goto zero;
-    }
-
-    v7 = ((a3 << 12) & 0xF000) | ((animType << 16) & 0xFF0000) | 0x1000000;
-    v8 = ((rotation << 28) & 0x70000000) | v7;
-    v9 = frmId & 0xFFF;
-
-    if (artExists(v9 | v8) != 0) {
-        goto out;
-    }
-
-    if (objectType == rotation) {
-        goto zero;
-    }
-
-    v10 = objectType;
-    if (artExists(v9 | v7 | 0x10000000) != 0) {
-        goto out;
-    }
-
-zero:
-
-    v10 = 0;
-
-out:
-
-    return ((v10 << 28) & 0x70000000) | (objectType << 24) | ((animType << 16) & 0xFF0000) | ((a3 << 12) & 0xF000) | (frmId & 0xFFF);
+    return buildFidInternal(frmId, weaponCode, animType, objectType, rotation);
 }
 
 // 0x419D60
