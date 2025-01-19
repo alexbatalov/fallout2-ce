acc52383ed3603eb10a6845113431591f0d92d22
root <root@localhost.localdomain>
2025-01-19 04:12:48 -0500

    emscripten: add CASE_INSENSITIVE_FS, move source map generation to debug mode

--- cmake/toolchain/Emscripten.cmake
+++ cmake/toolchain/Emscripten.cmake
@@ -1,13 +1,19 @@
 set( CMAKE_SYSTEM_NAME      "Emscripten" )
 
+add_compile_definitions("DEBUG=$<CONFIG:Debug>")
+
 set( CMAKE_C_COMPILER                   "emcc" )
 set( CMAKE_CXX_COMPILER                 "em++" )
-set( CMAKE_C_FLAGS                      "--use-port=sdl2 \
+set( CMAKE_C_FLAGS                      "${CMAKE_C_FLAGS} \
+                                        --use-port=sdl2 \
                                         --use-port=sdl2_mixer \
-                                        --use-port=zlib \
-                                        -gsource-map" )
-set( CMAKE_CXX_FLAGS                    "--use-port=sdl2 \
+                                        --use-port=zlib" )
+set( CMAKE_CXX_FLAGS                    "${CMAKE_CXX_FLAGS} \
+                                        --use-port=sdl2 \
                                         --use-port=sdl2_mixer \
-                                        --use-port=zlib \
-                                        -gsource-map" )
+                                        --use-port=zlib" )
+
+set( CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS} -gsource-map")
+set( CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS} -gsource-map")
+
 set( EXECUTABLE_NAME fallout2-ce.js )
