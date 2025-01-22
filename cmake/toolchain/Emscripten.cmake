31abd327fd39c73dff94f205192ddae21807f850
Roman Turchin <roman@turch.in>
2025-01-22 02:49:41 -0500

    emscripten: move options to CMakeLists.txt

--- cmake/toolchain/Emscripten.cmake
+++ cmake/toolchain/Emscripten.cmake
@@ -4,16 +4,5 @@ add_compile_definitions("DEBUG=$<CONFIG:Debug>")
 
 set( CMAKE_C_COMPILER                   "emcc" )
 set( CMAKE_CXX_COMPILER                 "em++" )
-set( CMAKE_C_FLAGS                      "${CMAKE_C_FLAGS} \
-                                        --use-port=sdl2 \
-                                        --use-port=sdl2_mixer \
-                                        --use-port=zlib" )
-set( CMAKE_CXX_FLAGS                    "${CMAKE_CXX_FLAGS} \
-                                        --use-port=sdl2 \
-                                        --use-port=sdl2_mixer \
-                                        --use-port=zlib" )
-
-set( CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS} -gsource-map")
-set( CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS} -gsource-map")
 
 set( EXECUTABLE_NAME fallout2-ce.js )
